e69a93e26c4052db7c940df517c750ecf7790d8b
Lukasz <oblak7@onet.pl>
2019-01-25 16:25:00 +0100

    Set canvas width relative to window size
    Add internal css to prevent default spacing so canvas can fit
    Center footer items arithmetically

--- sketch.js
+++ sketch.js
@@ -3,14 +3,15 @@ var addInput, addButton;
 
 function setup() {
     graph = new Graph();
-    createCanvas(1400, 600);
+    let footerHeight = 50;
+    createCanvas(innerWidth, innerHeight - footerHeight);
 
     addInput = createInput();
-    addInput.position(20, 20 + height);
+    addInput.position(20, innerHeight - footerHeight / 2 - addInput.height / 2);
     addButton = createButton('Add node');
-    addButton.position(25 + addInput.width, 20 + height);
+    addButton.position(25 + addInput.width, innerHeight - footerHeight / 2 - addButton.height / 2);
 
-    addButton.mousePressed(function(){
+    addButton.mousePressed(function () {
         graph.createNode(addInput.value(), 0, 0);
     });
 }
